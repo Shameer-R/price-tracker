c08e97fab5455c0919a64cf3e2af2a240c01284f
Shameer-R <roseshammy@gmail.com>
2025-07-02 17:51:30 -0700

    Load every product into handle_product function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+from pathlib import Path
 
 def convert_price_tag_to_string(price_tag):
     price_tag = price_tag.replace("$", "")
@@ -21,6 +23,7 @@ def get_website_name_from_url(website_url):
     else:
         raise ValueError(f"Invalid Website URL: {website_url}")
 
+# Reminder to self: Add mock-based test when product is nearing completion
 def get_product_price_from_url(website_url):
     if get_website_name_from_url(website_url) == "BestBuy":
         
@@ -33,3 +36,22 @@ def get_product_price_from_url(website_url):
         price_text = convert_price_tag_to_string(price_tag)
         
         return float(price_text)
+    
+# Run tasks on each product
+def handle_product(product):
+    product_name = product['name']
+    product_target_price = product['target_price']
+    product_sites = product['sites']
+    pass
+    
+if __name__ == "__main__":
+    root_dir = Path(__file__).resolve().parent.parent
+    config_path = root_dir / 'products.json'
+    
+    with open(config_path, 'r') as f:
+        parsed_json = json.load(f)
+        
+    product_list = parsed_json['products']
+    
+    for product in product_list:
+        handle_product(product)
